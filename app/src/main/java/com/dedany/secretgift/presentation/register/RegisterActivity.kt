5293a5bcece691b37c67deee6100d4cca623fdb1
GaleGizmo <juanmiguelabelleira@gmail.com>
2025-02-25 12:11:47 +0100

    update initObservers in Register

--- app/src/main/java/com/dedany/secretgift/presentation/register/RegisterActivity.kt
+++ app/src/main/java/com/dedany/secretgift/presentation/register/RegisterActivity.kt
@@ -1,10 +1,13 @@
 package com.dedany.secretgift.presentation.register
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.dedany.secretgift.R
 import com.dedany.secretgift.databinding.ActivityRegisterBinding
@@ -21,5 +24,51 @@ class RegisterActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[RegisterViewModel::class.java]
         setContentView(binding?.root)
 
+        initListeners()
+        initObservers()
+
+
+    }
+
+    private fun initObservers() {
+        viewModel?.nameError?.observe(this) {
+            binding?.nameLayout?.error = it
+        }
+        viewModel?.emailError?.observe(this) {
+            binding?.emailLayout?.error = it
+        }
+        viewModel?.passwordError?.observe(this) {
+            binding?.passwordLayout?.error = it
+        }
+        viewModel?.confirmPasswordError?.observe(this) {
+            binding?.confirmPasswordLayout?.error = it
+        }
+        viewModel?.isTermsAcceptedError?.observe(this) {
+            binding?.tvCheckboxError?.isVisible = !it.isNullOrEmpty()
+            binding?.tvCheckboxError?.text = it
+        }
+        viewModel?.isRegisterSuccessful?.observe(this) { isValid ->
+
+            Toast.makeText(
+                this,
+                if (isValid) "Registro exitoso" else "Error en el registro",
+                Toast.LENGTH_LONG
+            ).show()
+            if (isValid){
+                startActivity(Intent(this, HomeActivity::class.java))
+            }
+
+
+        }
+        viewModel?.formHasError?.observe(this) { hasError ->
+            if (hasError) {
+                Toast.makeText(this, "Error en el formulario", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/dedany/secretgift/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/dedany/secretgift/presentation/register/RegisterViewModel.kt
@@ -1,9 +1,42 @@
 package com.dedany.secretgift.presentation.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor():ViewModel() {
+    private var _isRegisterSuccessful: MutableLiveData<Boolean> = MutableLiveData()
+    val isRegisterSuccessful: LiveData<Boolean> = _isRegisterSuccessful
+    private var _formHasError: MutableLiveData<Boolean> = MutableLiveData()
+    val formHasError: LiveData<Boolean> = _formHasError
+
+    private var _name = MutableLiveData<String>("")
+    val name: LiveData<String> = _name
+
+    private var _email = MutableLiveData<String>("")
+    val email: LiveData<String> = _email
+
+    private var _password = MutableLiveData<String>("")
+    val password: LiveData<String> = _password
+    private var _confirmPassword = MutableLiveData<String>("")
+    val confirmPassword: LiveData<String> = _confirmPassword
+    private var _isTermsAccepted = MutableLiveData<Boolean>(false)
+    val isTermsAccepted: LiveData<Boolean> = _isTermsAccepted
+
+
+    private var _nameError: MutableLiveData<String?> = MutableLiveData()
+    val nameError: MutableLiveData<String?> = _nameError
+    private var _emailError: MutableLiveData<String?> = MutableLiveData()
+    val emailError: MutableLiveData<String?> = _emailError
+    private var _passwordError: MutableLiveData<String?> = MutableLiveData()
+    val passwordError: MutableLiveData<String?> = _passwordError
+    private var _confirmPasswordError: MutableLiveData<String?> = MutableLiveData()
+    val confirmPasswordError: MutableLiveData<String?> = _confirmPasswordError
+    private var _isTermsAcceptedError: MutableLiveData<String?> = MutableLiveData()
+    val isTermsAcceptedError: MutableLiveData<String?> = _isTermsAcceptedError
+
+
 }
