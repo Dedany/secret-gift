2fc3f05df2a91f87f4dcfd00081a4e97d91aaef8
everyojoden <danihernandezsilveira@gmail.com>
2025-03-07 16:06:04 +0100

    fix in main activity  fragment views

--- app/src/main/java/com/dedany/secretgift/presentation/main/MainActivity.kt
+++ app/src/main/java/com/dedany/secretgift/presentation/main/MainActivity.kt
@@ -2,23 +2,27 @@ package com.dedany.secretgift.presentation.main
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.dedany.secretgift.R
 import com.dedany.secretgift.databinding.ActivityMainBinding
 import com.dedany.secretgift.domain.entities.User
 import com.dedany.secretgift.presentation.fragments.MainFragment
 import com.dedany.secretgift.presentation.fragments.ProfileFragment
-import com.dedany.secretgift.presentation.helpers.getCustomSerializable
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var user: User  // Variable para almacenar al usuario
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    private lateinit var mainFragment: MainFragment
+    private lateinit var profileFragment: ProfileFragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,80 +30,95 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initAd()
-        user = intent.getCustomSerializable<User>("user") as? User ?: run {
-            Log.e("MainActivity", "El usuario es nulo")
-            return
-        }
-        val mainFragment = MainFragment()
-        val profileFragment = ProfileFragment()
-
-
+        observeViewModel()
 
         if (savedInstanceState == null) {
-            val bundle = Bundle()
-            bundle.putSerializable("user", user)  // Pasar el usuario al fragmento
-            mainFragment.arguments = bundle
-            profileFragment.arguments = bundle
-
-            supportFragmentManager.beginTransaction()
-                .replace(binding.fragmentContainer.id, mainFragment)
-                .commit()
-
+            viewModel.loadUser()
+            viewModel.loadGames()
+        } else {
+            restoreFragments() // Si la actividad se recrea, restauramos los fragmentos
         }
 
-        // Configurar la navegación del BottomNavigationView
-        binding.bottomNavigationMain.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    val bundle = Bundle()
-                    bundle.putSerializable("user", user)  // Pasar el usuario al fragmento
-                    mainFragment.arguments = bundle
+        setupBottomNavigation()
+    }
 
-                    supportFragmentManager.beginTransaction()
-                        .replace(binding.fragmentContainer.id, mainFragment)
-                        .commit()
-                    true
-                }
-
-                R.id.navigation_profile -> {
-                    val bundle = Bundle()
-                    bundle.putSerializable("user", user)  // Pasar el usuario al fragmento
-                    profileFragment.arguments = bundle
-
-                    supportFragmentManager.beginTransaction()
-                        .replace(binding.fragmentContainer.id, profileFragment)
-                        .commit()
-                    true
-                }
-
-                else -> false
+    private fun observeViewModel() {
+        viewModel.user.observe(this) { user ->
+            if (user != null) {
+                setupFragments(user)
+            } else {
+                Log.e("MainActivity", "El usuario es nulo")
             }
         }
 
     }
 
-    fun initAd() {
-        //iniciar adMob
-        MobileAds.initialize(this) { initialAd ->
-            val statusMap = initialAd.adapterStatusMap
-            for ((adapter, satatus) in statusMap) {
-                Log.d("AdMob", "Adapter: $adapter Status: ${satatus.description}")
+    private fun setupFragments(user: User) {
+        // Verificamos si los fragmentos ya existen en el FragmentManager
+        mainFragment = supportFragmentManager.findFragmentByTag("MainFragment") as? MainFragment
+            ?: MainFragment().apply {
+                arguments = Bundle().apply { putSerializable("user", user) }
+            }
+
+        profileFragment = supportFragmentManager.findFragmentByTag("ProfileFragment") as? ProfileFragment
+            ?: ProfileFragment().apply {
+                arguments = Bundle().apply { putSerializable("user", user) }
+            }
+
+        // Añadimos los fragmentos solo si no existen ya
+        if (!mainFragment.isAdded) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fragmentContainer.id, mainFragment, "MainFragment")
+                .commit()
+        }
+
+        if (!profileFragment.isAdded) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fragmentContainer.id, profileFragment, "ProfileFragment")
+                .hide(profileFragment)
+                .commit()
+        }
+    }
+
+    private fun restoreFragments() {
+        mainFragment = supportFragmentManager.findFragmentByTag("MainFragment") as? MainFragment
+            ?: MainFragment()
+        profileFragment = supportFragmentManager.findFragmentByTag("ProfileFragment") as? ProfileFragment
+            ?: ProfileFragment()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigationMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    showFragment(mainFragment)
+                    true
+                }
+                R.id.navigation_profile -> {
+                    showFragment(profileFragment)
+                    true
+                }
+                else -> false
             }
         }
-        binding?.adView?.let { adView ->
+    }
 
-            //asigna tamaño
-            /*adView.setAdSize(adSize)
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .hide(mainFragment)
+            .hide(profileFragment)
+            .show(fragment)
+            .commit()
+    }
 
-            // Configurar el ID
-            adView.adUnitId =
-                getString(R.string.admob_banner_id)*/
-
-            // Carga el anuncio
-            val adRequest = AdRequest.Builder().build()
-            adView.loadAd(adRequest)
-        } ?: run {
-            Log.e("AdMob", "adView es nulo.no hay anuncio")
+    private fun initAd() {
+        MobileAds.initialize(this) { initialAd ->
+            initialAd.adapterStatusMap.forEach { (adapter, status) ->
+                Log.d("AdMob", "Adapter: $adapter Status: ${status.description}")
+            }
         }
+
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
     }
 }
--- app/src/main/java/com/dedany/secretgift/presentation/main/MainActivityViewModel.kt
+++ app/src/main/java/com/dedany/secretgift/presentation/main/MainActivityViewModel.kt
@@ -1,28 +1,43 @@
 package com.dedany.secretgift.presentation.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dedany.secretgift.domain.entities.Game
+import com.dedany.secretgift.domain.entities.User
 import com.dedany.secretgift.domain.usecases.games.GamesUseCase
+import com.dedany.secretgift.domain.usecases.users.UsersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
-class MainActivityViewModel @Inject constructor(private val gamesUseCase: GamesUseCase) :
-    ViewModel() {
+class MainActivityViewModel @Inject constructor(
+    private val gamesUseCase: GamesUseCase,
+    private val usersUseCase: UsersUseCase
+) : ViewModel() {
 
     private val _games: MutableLiveData<List<Game>?> = MutableLiveData()
     val games: LiveData<List<Game>?> = _games
+
+    private val _user: MutableLiveData<User> = MutableLiveData()
+    val user: LiveData<User> = _user
 
     fun loadGames() {
         viewModelScope.launch {
             _games.value = gamesUseCase.getGamesByUser()
         }
     }
+
+    fun loadUser() {
+        viewModelScope.launch {
+            val registeredUser = usersUseCase.getRegisteredUser()
+            _user.postValue(registeredUser)
+    }
+}}
 
 //    fun updateGamesList(position: Int, games: Game) {
 //        _games.value?.get(position)?.apply {
@@ -42,4 +57,4 @@ class MainActivityViewModel @Inject constructor(private val gamesUseCase: GamesU
 //            _games.value = updatedGames
 //        }
 //    }
-}
+
